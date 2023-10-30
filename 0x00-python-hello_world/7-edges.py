8c43dffffd2137535d0132412bc973975955f612
moyasser20 <mohamedyasser192023@gmail.com>
2023-10-30 14:34:58 -0700

    g

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+wordfirst3 = word[:3]
+wordlast2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters: {}".format(wordfirst3))
+print("Last 2 letters: {}".format(wordlast2))
+print("Middle word: {}".format(middle_word))
