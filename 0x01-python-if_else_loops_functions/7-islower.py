0182d0e8138caa1a0c099b9d65fa35eeb702582c
moyasser20 <mohamedyasser192023@gmail.com>
2023-10-31 03:13:09 -0700

    g

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Write function that prints string in lowercase followed by a new line."""
 
-def islower(strr):
-
-
-    if ord(strr) >= 97 and ord(strr) <= 122:
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
