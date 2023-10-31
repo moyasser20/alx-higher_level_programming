97e5e668009b8583f1dce333aa0be1137417e36b
moyasser20 <mohamedyasser192023@gmail.com>
2023-10-31 01:49:06 -0700

    g

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Print the alphabet in lowercase except q and e , not followed by a new line."""
+
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print(chr(letter), end="")
