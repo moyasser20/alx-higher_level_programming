02aed8e848d66ab2d33d6b7e7917c1153f21141d
moyasser20 <mohamedyasser192023@gmail.com>
2023-10-31 03:03:43 -0700

    g

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Write program prints all possible different combinations of two digits."""
 for d1 in range(0, 10):
-    for d2 in range(d1 + 1 ,10):
+    for d2 in range(d1 + 1, 10):
         if d1 == 8 and d2 == 9:
             print("{}{}".format(d1,d2))
         else:
