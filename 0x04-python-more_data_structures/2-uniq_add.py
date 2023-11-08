2c3121c878516fe764cb98fdcba220e7272f2bb3
moyasser20 <mohamedyasser192023@gmail.com>
2023-11-08 01:18:06 -0800

    g

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def uniq_add(my_list=[]):
 
     newl = []
     sum = 0
 
     for num in my_list:
-        if num not int newl:
+        if num not in newl:
             sum += num
             newl.append(num)
     return sum
