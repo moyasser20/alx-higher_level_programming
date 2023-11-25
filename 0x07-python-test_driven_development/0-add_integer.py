3ead29d5ddd7c38edb05ce6a4b24d034c8c36830
moyasser20 <mohamedyasser192023@gmail.com>
2023-11-25 03:38:25 -0800

    g

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines an integer addition function."""
+
+def add_integer(a, b=98):
+    """Return the integer addtion of a and b.
+
+    float arguments
+
+    Raises:
+    TypeError: If eitehr of a or b is a non-integer and non-float.
+    """
+
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int , float):
+        raise TypeError("b must be and integer")
+    return int(a) + int(b)
+
+if __name__ = "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
